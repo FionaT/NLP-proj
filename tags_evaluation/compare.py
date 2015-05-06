f1cae89ad2e81246de36deb92d78a981a49ae670
Fiona <fionamood@gmail.com>
2015-05-06 13:22:14 -0400

    update

--- tags_evaluation/compare.py
+++ tags_evaluation/compare.py
@@ -11,9 +11,12 @@ business_attr = []
 
 count = 0
 
+#get the overlap_tags from yelp attributes set and our tags set
 for line in overlap_tags_file:
 	overlap_tags = line.split(",")
 
+#get all business and its attributes from yelp data
+#map the business id to its index for furture use
 for line in yelp_file:
 	line = line.split(",")
 	line = line[:-1]
@@ -21,27 +24,36 @@ for line in yelp_file:
 	find_index[line[0]] = count
 	count += 1
 
+#initialize the variables
 count = 0
 precision = 0
 recall = 0
 
-for line in our_file:
-	
+total_corrects = 0
+total_tags1 = 0
+total_tags2 = 0
 
+for line in our_file:
+
+	#read our result file about predicted business tags
 	line = line.split(",")
 	line = line[:-1]
 
 	index = find_index[line[0]]
+
+	#set1 is the business attributes from yelp
 	set1 = business_attr[index][1:]
 
+	#calculate the intersection of set1 and overlap_tags
+	#overlap_tags is the tags set that our system share with yelp attributes set
 	set1 = set(set1).intersection(overlap_tags)
 
-	#if(len(set1) == 0):
-	#	continue
-
-	
-
+	#set1 is the business tags from our result
 	set2 = line[1:]
+
+	#calculate the intersection of set2 and overlap_tags
+	#overlap_tags is the tags set that our system share with yelp attributes set
+	set2 = set(set2).intersection(overlap_tags)
 
 	overlap = set(set1).intersection(set2)
 
@@ -54,24 +66,28 @@ for line in our_file:
 	key = len(set1)
 	correct = len(overlap)
 
-	print business_attr[index][0], set1
-	print line[0], set2
+	total_corrects += correct
+	total_tags1 += response
+	total_tags2 += key
+
+	#print business_attr[index][0], set1
+	#print line[0], set2
 	
 	if(response == 0):
 		precision += 0
 	else:
 		precision += correct / float(response)
-		print correct / float(response)
+		#print correct / float(response)
 	
 	if(key == 0):
 		recall += 0
 	else:
 		recall += correct / float(key)
-		print correct / float(key)
+		#print correct / float(key)
 
 	#print precision, recall
 
 print precision/count , recall/count
-
+print total_corrects / float(total_tags1), total_corrects / float(total_tags2)
 
 	
