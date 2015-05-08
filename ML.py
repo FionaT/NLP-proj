c8c67427ca3760d7f00035e084a50c25c97ba177
Joshua924 <sz1288@nyu.edu>
2015-05-08 00:34:33 -0400

    changed

--- ML.py
+++ ML.py
@@ -7,12 +7,12 @@ from nltk.stem.wordnet import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import RandomForestClassifier
 
-fin = open('/Users/dengjingwen/Courses/NLP/manual_read/sentens_samples.txt', 'r')
+fin = open('sentens_samples.txt', 'r')
 texts = fin.readlines()
 tfidf = TfidfVectorizer()
 x = tfidf.fit_transform(texts)
 fin.close()
-tv = open('/Users/dengjingwen/Courses/NLP/manual_read/_target_values.txt', 'r')
+tv = open('_target_values.txt', 'r')
 y = tv.readlines()
 classifier = RandomForestClassifier()
 classifier.fit(x, y)
@@ -43,10 +43,10 @@ def getFeatures(s):
     test2 = [test2]
     result1 = classifier.predict(tfidf.transform(test1))
     result2 = classifier.predict(tfidf.transform(test2))
-    print(test1)
-    print(result1)
-    print(test2)
-    print(result2)
+#    print(test1)
+#    print(result1)
+#    print(test2)
+#    print(result2)
     for t in result1:
         features.append(t[:-1])
     if p != '':
@@ -71,8 +71,3 @@ def extractFeatures(line):
         if not f in features:
             features.append(f)
     return features
-
-#test = ['I hate that it\'s cash only', 'service excellent time ordered whole pie', 'support parking']
-#response = tfidf.transform(test)
-#print(classifier.predict(response))
-print(extractFeatures('I don\'t like them that they do not accept credit card. Charlotte Meeting Room is a modern and new place for holding events and business meetings. They have state of the art equipment included in the price. We held network training classes there for 30 people. The high wattage outlets were stable enough to power all our equipment and computers without any glitches. Internet bandwidth is also great. We used Wifi for most of our work. The business park is very professional and upscale with plenty of free parking.'))
